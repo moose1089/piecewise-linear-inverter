be6a52ae6374bde43f5f083e0f950051c2465ab1
Mark Mestern <mark.mestern@uswitch.com>
2019-06-19 12:34:48 +0100

    simpler

--- src/inverter/core.clj
+++ src/inverter/core.clj
@@ -2,36 +2,37 @@
   (:require [clojure.tools.trace])
   (:gen-class))
 
+;; NOTE PWLF = Piece-wise linear function
+
 ;; TODO assert thresholds starts 0
-(defn fwd
+(defn eval-PWLF
   [{:keys [x-intercept y-intercept thresholds slopes] :as d} x]
   {:pre [(= (count slopes) (count thresholds))]}
   (cond
-    (not (zero? x-intercept)) (fwd (-> d (assoc :x-intercept 0)) (- x x-intercept))
-    (not (zero? y-intercept)) (+ y-intercept (fwd (-> d (assoc :y-intercept 0)) x))
+    (not (zero? x-intercept)) (eval-PWLF (-> d (assoc :x-intercept 0)) (- x x-intercept))
+    (not (zero? y-intercept)) (+ y-intercept (eval-PWLF (-> d (assoc :y-intercept 0)) x))
     (< x 0)                   (throw (ex-info (str "out out scope" {:x x :d d})  {:x x :d d}))
-    (zero? x)                 0
     (= 1 (count thresholds))  (+ y-intercept (* (first slopes) x))
     (< 1 (count thresholds))  (let [x-used (min x (- (second thresholds) (first thresholds)))
                                     new-d  (-> d
                                               (update :slopes rest)
                                               (update :thresholds rest)
                                               (assoc :y-intercept 0))]
-                                (+ y-intercept (* (first slopes) x-used) (fwd new-d (- x x-used)))))
+                                (+ (* (first slopes) x-used) (eval-PWLF new-d (- x x-used)))))
 )
 
-(defn invert
+(defn invert-PWLF
   [d]
   {:x-intercept (:y-intercept d)
    :y-intercept (:x-intercept d)
    :slopes      (map #(/ 1 %) (:slopes d))
    :thresholds  (map (fn [x]
-                       (- (fwd d (+ x (:x-intercept d))) (:y-intercept d)))
+                       (- (eval-PWLF d (+ x (:x-intercept d))) (:y-intercept d)))
                      (:thresholds d))}
   )
 
-(defn back [d x]
-  (fwd (invert d) x)
+(defn eval-inverse-PWLF [d x]
+  (eval-PWLF (invert-PWLF d) x)
   )
 
 (defn -main
--- test/inverter/core_test.clj
+++ test/inverter/core_test.clj
@@ -6,76 +6,75 @@
   (testing "single segment"
     (let [d {:x-intercept 0 :y-intercept 2 :thresholds [0] :slopes [1]}]
       (doseq [i (range 0 100 2)]
-        (is (= (back d (fwd d i)) i)))
+        (is (= (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "multi segment, no intercept, constant slopes"
     (let [d {:x-intercept 0 :y-intercept 0 :thresholds [0 2 5 8] :slopes [1 1 1 1]} ]
       (doseq [i (range 0 100 1)]
-          (is         (== (back d (fwd d i)) i)))
+          (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "multi segment, no intercept, changing slopes"
     (let [d {:x-intercept 0 :y-intercept 0 :thresholds [0 2 5 8] :slopes [1 2 3 0.25]} ]
       (doseq [i (range 0 100 1)]
-          (is         (== (back d (fwd d i)) i)))
+          (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "multi segment, y intercept, changing slopes"
     (let [d {:x-intercept 0 :y-intercept 10 :thresholds [0 2 5 8] :slopes [1 2 3 0.25]} ]
       (doseq [i (range 0 100 22)]
-        (is         (== (back d (fwd d i)) i)))
+        (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "single segment, x intercept"
     (let [d {:x-intercept 10 :y-intercept 0 :thresholds [0] :slopes [1]} ]
-      (is         (== (back d (fwd d 19)) 19))
-      (is         (== (back d (fwd d 66)) 66))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 19)) 19))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 66)) 66))
       (doseq [i (range 10 100 22)]
-        (is         (== (back d (fwd d i)) i)))
+        (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "multi segment, x intercept, constant slopes"
     (let [d {:x-intercept 10 :y-intercept 0 :thresholds [0 2 5 8] :slopes [1 1 1 1]} ]
-      (is         (== (back d (fwd d 19)) 19))
-      (is         (== (back d (fwd d 66)) 66))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 19)) 19))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 66)) 66))
       (doseq [i (range 10 100 22)]
-        (is         (== (back d (fwd d i)) i)))
+        (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "multi segment, x intercept, changing slopes"
     (let [d {:x-intercept 5 :y-intercept 0 :thresholds [0 2 5 8] :slopes [1 2 3 0.25]} ]
-      (is         (== (back d (fwd d 9)) 9))
-      (is         (== (back d (fwd d 20)) 20))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 9)) 9))
+      (is         (== (eval-inverse-PWLF d (eval-PWLF d 20)) 20))
       (doseq [i (range 10 100 22)]
-        (is         (== (back d (fwd d i)) i)))
+        (is         (== (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
-
 
   (testing "double segment"
     (let [d {:x-intercept 0 :y-intercept 2 :thresholds [0 3] :slopes [2 (/ 1 3)]} ]
-      (is (= (fwd d 3) 8))
-      (is (= (fwd d 9) 10))
-      (is         (= (back d (fwd d 9)) 9))
+      (is (= (eval-PWLF d 3) 8))
+      (is (= (eval-PWLF d 9) 10))
+      (is         (= (eval-inverse-PWLF d (eval-PWLF d 9)) 9))
       (doseq [i (range 0 100 22)]
-        (is         (= (back d (fwd d i)) i)))
+        (is         (= (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "double segment x int"
     (let [d {:x-intercept 2 :y-intercept 0 :thresholds [0 3] :slopes [2 (/ 1 3)]} ]
-      (is (= (fwd d 3) 2))
-      (is (= (fwd d 8) 7))
-      (is         (= (back d (fwd d 9)) 9))
+      (is (= (eval-PWLF d 3) 2))
+      (is (= (eval-PWLF d 8) 7))
+      (is         (= (eval-inverse-PWLF d (eval-PWLF d 9)) 9))
       (doseq [i (range 3 100 22)]
-        (is         (= (back d (fwd d i)) i)))
+        (is         (= (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       ))
 
   (testing "double segment other direction "
-    (let [d (invert {:x-intercept 0 :y-intercept 2 :thresholds [0 3] :slopes [2 (/ 1 3)]})]
-      (is (= (back d (fwd d 2)) 2) )
-      (is (= (fwd d 8) 3))
-      (is (= (fwd d 10) 9))
-      (is (= (back d (fwd d 9)) 9))
+    (let [d (invert-PWLF {:x-intercept 0 :y-intercept 2 :thresholds [0 3] :slopes [2 (/ 1 3)]})]
+      (is (= (eval-inverse-PWLF d (eval-PWLF d 2)) 2) )
+      (is (= (eval-PWLF d 8) 3))
+      (is (= (eval-PWLF d 10) 9))
+      (is (= (eval-inverse-PWLF d (eval-PWLF d 9)) 9))
       (doseq [i (range 2 100 22)]
-        (is         (= (back d (fwd d i)) i)))
+        (is         (= (eval-inverse-PWLF d (eval-PWLF d i)) i)))
       )))
